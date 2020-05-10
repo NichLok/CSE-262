5118bb2028b00cedffe11cef0e2b055afa02e6ec
Nicholas <nicholaslok1996@gmail.com>
2020-05-10 10:50:08 +0800

    Homework 2 updated

---
+++ Homework 2/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod variables; //done and tests passed
+pub mod strings; //done and tests passed
+pub mod primitive_types; //done and tests passed
+pub mod control; //done
+pub mod functions; //done and tests passed, only issue is that it says "function called but never used"
+pub mod structs; //done and test passed
+pub mod move_semantics; //done and tests passed
